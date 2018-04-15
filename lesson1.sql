e3864ee1f7f3e07927334ff32423465f3129915e
Serena Stater <srstater@gmail.com>
2018-04-15 11:35:10 -0400

    finish L1

--- lesson1.sql
+++ lesson1.sql
@@ -4,6 +4,13 @@ SELECT occurred_at, account_id, channel /* which columns */
 	WHERE account_id = 4251 /* place after from but before ORDER BY or LIMIT; if string, surround with quotes */
 	WHERE referrer_url LIKE '/%google%/' /* anything where url contains google; opeartor requires wild cards: % % */
 	WHERE name IN ('Walmart', 'Apple')
+	WHERE sales_rep_id NOT IN (321500, 321570) /* opposite */
+	WHERE referrer_url NOT LIKE '/%google%/' /* opposite */
+	WHERE occurred_at >= '2016-04-01' AND occurred_at <= '2016-10-01' /* AND used within WHERE statement. Each time you link a new statement with an AND, you will need to specify the column you are interested in looking at. This operator works with all of the operations we have seen so far including arithmetic operators (+, *, -, /). LIKE, IN, and NOT logic can also be linked together using the AND operator */
+	WHERE column BETWEEN 6 AND 10
+	WHERE standard_qty = 0 OR gloss_qty = 0 /* or means one or the other is true */
+	WHERE (standard_qty = 0 OR gloss_qty = 0) /* parentheses make one logical statement that resolves to true or false. as long as ONE statement is true, the entire block in parentheses is considered to be true */
+
 	ORDER BY occurred_at, account_id DESC channel /* must appear after FROM and before LIMIT. DESC should be last */
   LIMIT 15; /* limit to first 15 rows */
 
@@ -81,3 +88,86 @@ All companies whose names end with 's'.
 SELECT name
 FROM accounts
 WHERE name LIKE '%S'
+
+Use the accounts table to find the account name, primary_poc, and sales_rep_id for Walmart, Target, and Nordstrom.
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name IN ('Walmart', 'Target', 'Nordstrom')
+
+Use the web_events table to find all information regarding individuals who were contacted via the channel of organic or adwords.
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords')
+
+Use the accounts table to find the account name, primary poc, and sales rep id for all stores except Walmart, Target, and Nordstrom.
+
+SELECT name, primary_poc, sales_rep_id
+FROM accounts
+WHERE name NOT IN ('Walmart', 'Target', 'Google')
+
+Use the web_events table to find all information regarding individuals who were contacted via any method except using organic or adwords methods.
+
+SELECT *
+FROM web_events
+WHERE channel NOT IN ('organic', 'adwords')
+
+Use the accounts table to find:
+all the companies whose names do not start with 'C'.
+
+SELECT *
+FROM accounts
+WHERE name NOT IN ('C%');
+
+All companies whose names do not contain the string 'one' somewhere in the name.
+
+SELECT *
+FROM accounts
+WHERE name NOT IN ('%one%');
+
+All companies whose names do not end with 's'.
+
+SELECT *
+FROM accounts
+WHERE name NOT IN ('%s');
+
+Write a query that returns all the orders where the standard_qty is over 1000, the poster_qty is 0, and the gloss_qty is 0.
+
+SELECT *
+FROM orders
+WHERE standard_qty > 1000 AND poster_qty = 0 AND gloss_qty = 0;
+
+-- Using the accounts table find all the companies whose names do not start with 'C' and end with 's'.
+
+SELECT *
+FROM accounts
+WHERE name NOT IN ('C%') AND name IN ('%s');
+
+-- Use the web_events table to find all information regarding individuals who were contacted via organic or adwords and started their account at any point in 2016 sorted from newest to oldest.
+
+SELECT *
+FROM web_events
+WHERE channel IN ('organic', 'adwords') AND occurred_at BETWEEN '2016-01-01' AND '2016-12-31'
+ORDER BY occurred_at DESC;
+
+-- Find list of orders ids where either gloss_qty or poster_qty is greater than 4000. Only include the id field in the resulting table.
+
+SELECT id
+FROM orders
+WHERE gloss_qty > 4000 OR poster_qty > 4000
+
+-- Write a query that returns a list of orders where the standard_qty is zero and either the gloss_qty or poster_qty is over 1000.
+
+SELECT *
+FROM orders
+WHERE standard_qty = 0
+	AND (gloss_qty > 1000 OR poster_qty > 1000);
+
+-- Find all the company names that start with a 'C' or 'W', and the primary contact contains 'ana' or 'Ana', but it doesn't contain 'eana'.
+
+SELECT *
+FROM accounts
+WHERE (name LIKE 'C%' OR name LIKE 'W%')
+AND ((primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%')
+AND primary_poc NOT LIKE '%eana%';
