78ea8fc1e56afe3d11400ad18c4798b1a7ba756a
Serena Stater <srstater@gmail.com>
2018-04-25 18:09:49 -0400

    more python

--- lesson1.sql
+++ lesson1.sql
@@ -10,9 +10,21 @@ SELECT occurred_at, account_id, channel /* which columns */
 	WHERE column BETWEEN 6 AND 10
 	WHERE standard_qty = 0 OR gloss_qty = 0 /* or means one or the other is true */
 	WHERE (standard_qty = 0 OR gloss_qty = 0) /* parentheses make one logical statement that resolves to true or false. as long as ONE statement is true, the entire block in parentheses is considered to be true */
-
 	ORDER BY occurred_at, account_id DESC channel /* must appear after FROM and before LIMIT. DESC should be last */
   LIMIT 15; /* limit to first 15 rows */
+
+/* | statement | how to use it | other details | */
+SELECT	SELECT Col1, Col2, ...	Provide the columns you want
+FROM	FROM Table	Provide the table where the columns exist
+LIMIT	LIMIT 10	Limits based number of rows returned
+ORDER BY	ORDER BY Col	Orders table based on the column. Used with DESC.
+WHERE	WHERE Col > 5	A conditional statement to filter your results
+LIKE	WHERE Col LIKE '%me%	Only pulls rows where column has 'me' within the text
+IN	WHERE Col IN ('Y', 'N')	A filter for only rows with column of 'Y' or 'N'
+NOT	WHERE Col NOT IN ('Y', "N')	NOT is frequently used with LIKE and IN
+AND	WHERE Col1 > 5 AND Col2 < 3	Filter rows where two or more conditions must be true
+OR	WHERE Col1 > 5 OR Col2 < 3	Filter rows where at least one condition must be true
+BETWEEN	WHERE Col BETWEEN 3 AND 5	Often easier syntax than using an AND
 
 /* Logical operators */
 LIKE
--- lesson2.py
+++ lesson2.py
@@ -47,3 +47,160 @@ print(name_length)
 # Nothing you need to do here
 driving_license_character_limit = 28
 print(name_length <= driving_license_character_limit)
+
+# '''
+# You decide you want to play a game where you are hiding
+# a number from someone.  Store this number in a variable
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+answer = 7
+guess = 9
+
+if guess < answer:
+    result = "Oops!  Your guess was too low."
+elif guess > answer:
+    result = "Oops!  Your guess was too high."
+elif answer == guess:
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
+
+# '''
+# Depending on where an individual is from we need to tax them
+# appropriately.  The states of CA, MN, and
+# NY have taxes of 7.5%, 9.5%, and 8.9% respectively.
+# Use this information to take the amount of a purchase and
+# the corresponding state to assure that they are taxed by the right
+# amount.
+# '''
+state = 'CA'
+purchase_amount = 100
+
+if state == 'CA':
+    tax_amount = .075
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == 'MN':
+    tax_amount = .095
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+elif state == 'NY':
+    tax_amount = .089
+    total_cost = purchase_amount*(1+tax_amount)
+    result = "Since you're from {}, your total cost is {}.".format(state, total_cost)
+
+print(result)
+
+points = 174
+
+# if points <= 50:
+#     result = "Congratulations! You won a wooden rabbit!"
+# elif points <= 150:
+#     result = "Oh dear, no prize this time."
+# elif points <= 180:
+#     result = "Congratulations! You won a wafer-thin mint!"
+# else:
+#     result = "Congratulations! You won a penguin!"
+#
+# print(result)
+
+# You will use a new variable prize to store a prize name if one was won, and then use the truth value of this variable to compose the result message. This will involve two if statements.
+#
+# 1st conditional statement: update prize to the correct prize name based on points.
+# 2nd conditional statement: set result to the correct phrase based on whether prize is evaluated as True or False.
+#
+# If prize is None, result should be set to "Oh dear, no prize this time."
+# If prize contains a prize name, result should be set to "Congratulations! You won a {}!".format(prize). This will avoid having the multiple result assignments for different prizes.
+# At the beginning of your code, set prize to None, as the default value.
+
+points = 174  # use this input when submitting your answer
+
+# set prize to default value of None
+prize = None
+
+# use the value of points to assign prize to the correct prize name
+if points <= 50:
+    prize = "wooden rabbit"
+elif 151 <= points <= 180:
+    prize = "wafer-thin mint"
+elif points >= 181:
+    prize = "penguin"
+
+# use the truth value of prize to assign result to the correct message
+if prize:
+    result = "Congratulations! You won a {}!".format(prize)
+else:
+    result = "Oh dear, no prize this time."
+
+print(result)
+
+# Write a for loop that iterates over the names list to create a usernames list. To create a username for each name, make everything lowercase and replace spaces with underscores. Running your for loop over the list:
+#
+# names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+#
+# should create the list:
+#
+# usernames = ["joey_tribbiani", "monica_geller", "chandler_bing", "phoebe_buffay"]
+
+names = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+usernames = []
+
+# write your for loop here
+for name in names:
+    usernames.append(name.lower().replace(" ","_"))
+
+print(usernames)
+
+# Write a for loop that uses range() to iterate over the positions in usernames to modify the list. Like you did in the previous quiz, change each name to be lowercase and replace spaces with underscores. After running your loop, this list
+#
+# usernames = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+#
+# should change to this:
+#
+# usernames = ["joey_tribbiani", "monica_geller", "chandler_bing", "phoebe_buffay"]
+
+usernames = ["Joey Tribbiani", "Monica Geller", "Chandler Bing", "Phoebe Buffay"]
+
+# write your for loop here
+for index in range(len(usernames)):
+    usernames[index] = usernames[index].lower().replace(" ","_")
+
+print(usernames)
+
+# Write a for loop that iterates over a list of strings, tokens, and counts how many of them are XML tags. XML is a data language similar to HTML. You can tell if a string is an XML tag if it begins with a left angle bracket "<" and ends with a right angle bracket ">". Keep track of the number of tags using the variable count.
+#
+# You can assume that the list of strings will not contain empty strings.
+
+tokens = ['<greeting>', 'Hello World!', '</greeting>']
+
+# write your for loop here
+count = 0
+for token in tokens:
+     if token[0] == '<' and token[-1] == '>':
+         count +=1
+
+print(count)
+
+# Write a for loop that iterates over a list of strings and creates a single string, html_str, which is an HTML list. For example, if the list is items = ['first string', 'second string'], printing html_str should output:
+#
+# <ul>
+# <li>first string</li>
+# <li>second string</li>
+# </ul>
+# That is, the string's first line should be the opening tag <ul>. Following that is one line per element in the source list, surrounded by <li> and </li> tags. The final line of the string should be the closing tag </ul>.
+
+items = ['first string', 'second string']
+
+html_str = "<ul>\n"
+for item in items:
+    html_str += "<li>{}</li>\n".format(item)
+html_str += "</ul>"
+
+print(html_str)
