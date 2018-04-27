073082ceb594ac529e6f32bb1311346e09133c10
Serena Stater <srstater@gmail.com>
2018-04-26 20:45:17 -0400

    more python

--- lesson2.py
+++ lesson2.py
@@ -204,3 +204,75 @@ for item in items:
 html_str += "</ul>"
 
 print(html_str)
+
+# You would like to count the number of fruits in your basket.
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits, but you do not want to count the other items in your basket.
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwhiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+#if the key is in the list of fruits, add the value (number of fruits) to result
+for fruit, count in basket_items.items():
+   if fruit in fruits:
+       result += count
+
+print("There are {} fruits in the basket".format(result))
+
+# You would like to count the number of fruits in your basket.
+# In order to do this, you have the following dictionary and list of
+# fruits.  Use the dictionary and list to count the total number
+# of fruits and not_fruits.
+
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwhiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+#if the key is in the list of fruits, add to fruit_count.
+
+fruit_count, not_fruit_count = 0, 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwhiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+#Iterate through the dictionary
+for fruit, count in basket_items.items():
+    if fruit in fruits:
+       fruit_count += count
+    else:
+        not_fruit_count += count
+
+print("The number of fruits is {}.  There are {} items that are not fruits.".format(fruit_count, not_fruit_count))
+
+# Write a while loop that finds the largest square number less than an integerlimit and stores it in a variable nearest_square. A square number is the product of an integer multiplied by itself, for example 36 is a square number because it equals 6*6.
+#
+# For example, if limit is 40, your code should set the nearest_square to 36.
+
+limit = 40
+
+num = 0
+while (num+1)**2 < limit:
+    num += 1
+nearest_square = num**2
+
+print(nearest_square)
+
+# Consider you want to count from some number `start_num` by
+# another number `count_by` until you hit a final number `end_num`.
+# Use the quiz below to put this idea to work.
+
+start_num = 5
+end_num = 100
+count_by = 2
+
+
+break_num = start_num
+while break_num <= end_num:
+    break_num += count_by
+
+break_num -= count_by
+
+print(break_num)
