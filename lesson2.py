b2fe7e35b2a95e28aa2ecc2972f758dfdaba7361
Serena Stater <srstater@gmail.com>
2018-04-28 15:17:42 -0400

    finishing python into

--- lesson2.py
+++ lesson2.py
@@ -396,3 +396,94 @@ scores = {
 
 passed = [name for name, score in scores.items() if score >= 65]
 print(passed)
+
+# Write a function named readable_timedelta. The function should take one argument, an integer days, and return a string that says how many weeks and days that is. For example, calling the function and printing the result like this:
+#
+# print(readable_timedelta(10))
+# should output the following:
+#
+# 1 week(s) and 3 day(s).
+
+def readable_timedelta(days):
+    weeks = days // 7
+    remainder = days % 7
+    return "{} week(s) and {} day(s).".format(weeks, remainder)
+
+# test your function
+print(readable_timedelta(10))
+
+# map() is a higher-order built-in function that takes a function and iterable as inputs, and returns an iterator that applies the function to each element of the iterable. The code below uses map() to find the mean of each list in numbers to create the list averages. Test run it to see what happens.
+#
+# Rewrite this code to be more concise by replacing the mean function with a lambda expression defined within the call to map().
+
+numbers = [
+              [34, 63, 88, 71, 29],
+              [90, 78, 51, 27, 45],
+              [63, 37, 85, 46, 22],
+              [51, 22, 34, 11, 18]
+           ]
+
+averages = list(map(lambda x: sum(x) / len(x), numbers))
+print(averages)
+
+# filter() is a higher-order built-in function that takes a function and iterable as inputs and returns an iterator with the elements from the iterable for which the function returns True. The code below uses filter() to get the names in cities that are fewer than 10 characters long to create the list short_cities. Give it a test run to see what happens.
+#
+# Rewrite this code to be more concise by replacing the is_short function with a lambda expression defined within the call to filter().
+
+cities = ["New York City", "Los Angeles", "Chicago", "Mountain View", "Denver", "Boston"]
+
+short_cities = list(filter(lambda x: len(x) < 10, cities))
+print(short_cities)
+
+# Write your own generator function that works like the built-in function enumerate.
+#
+# Calling the function like this:
+#
+# lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+#
+# for i, lesson in my_enumerate(lessons, 1):
+#     print("Lesson {}: {}".format(i, lesson))
+# should output:
+#
+# Lesson 1: Why Python Programming
+# Lesson 2: Data Types and Operators
+# Lesson 3: Control Flow
+# Lesson 4: Functions
+# Lesson 5: Scripting
+
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    count = start
+    for element in iterable:
+        yield count, element
+        count += 1
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
+
+# If you have an iterable that is too large to fit in memory in full (e.g., when dealing with large files), being able to take and use chunks of it at a time can be very valuable.
+#
+# Implement a generator function, chunker, that takes in an iterable and yields a chunk of a specified size at a time.
+#
+# Calling the function like this:
+#
+# for chunk in chunker(range(25), 4):
+#     print(list(chunk))
+# should output:
+#
+# [0, 1, 2, 3]
+# [4, 5, 6, 7]
+# [8, 9, 10, 11]
+# [12, 13, 14, 15]
+# [16, 17, 18, 19]
+# [20, 21, 22, 23]
+# [24]
+
+def chunker(iterable, size):
+    """Yield successive chunks from iterable of length size."""
+    for i in range(0, len(iterable), size):
+        yield iterable[i:i + size]
+
+for chunk in chunker(range(25), 4):
+    print(list(chunk)) 
